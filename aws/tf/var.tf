525dc6986027940f328be336456917a48953027f
lee <lee@chaui-MacBookPro.local>
2024-02-29 03:05:17 +0900

    test

--- aws/tf/var.tf
+++ aws/tf/var.tf
@@ -1,14 +1,14 @@
 variable "vpc_id" {
   type = string
-  default = "vpc-0aff28fe1b97da9de"
+  default = "vpc-01280e74e795d9175"
 }
 
 variable "public-subnet" {
   type = list(string)
-  default = [ "subnet-01e6e172e18669f8c" ]
+  default = [ "subnet-0e745a1268de4f71c" ]
 }
 
 variable "private-subnet" {
   type = list(string)
-  default = [ "subnet-000e1982db8d4479f" ]
+  default = [ "subnet-0713f5d87f8aa8945" ]
 }
