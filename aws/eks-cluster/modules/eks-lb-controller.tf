38f3b612a1d2b30b1b2ee4aba8ea3c3d863181e5
leesanghoon94 <isupperhoon@gmail.com>
2024-07-10 04:12:23 +0900

    추가 eks-lb-controller.tf

--- aws/eks-cluster/modules/eks-lb-controller.tf
+++ aws/eks-cluster/modules/eks-lb-controller.tf
@@ -1,4 +1,3 @@
-#https://kubernetes-sigs.github.io/aws-load-balancer-controller/v2.8/deploy/installation/
 #AWS Load Balancer Controller v2.5.0+ requires Kubernetes 1.22+
 # Download an IAM policy for the LBC using one of the following commands:
 # If your cluster is in any other region:
@@ -7,6 +6,24 @@
 # Optionally download the default ingressclass and ingressclass params
 #wget https://github.com/kubernetes-sigs/aws-load-balancer-controller/releases/download/v2.8.1/v2_8_1_ingclass.yaml
 
-module "iam_assume_role_alb_controller" {
+module "iam_assumable_role_with_oidc" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
 
+  create_role                   = true
+  role_name                     = "lb-controller-role"
+  provider_url                  = module.eks.oidc_provider_url
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:aws-load-balancer-controller"]
+
+
+}
+
+
+data "http" "iam_policy" {
+  url = "https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.8.1/docs/install/iam_policy.json"
+}
+
+resource "aws_iam_role_policy" "lb_controller" {
+  name_prefix = "AWSLoadBalancerControllerIAMPolicy"
+  policy      = data.http.iam_policy.body
+  role        = module.iam_assumable_role_alb_controller.iam_role_name
 }
